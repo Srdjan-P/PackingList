e975cc3b9012cdd9a2e99872ad7832d1faa0b4a1
Srdjan-P <srdjan.potrebic@gmail.com>
2025-05-18 15:21:02 +0200

    add: animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 export default function Header() {
   return (
     <header>
-      <div className="logo-container"></div>
+      <div className="logo-container">
+        <div className="logo"></div>
+        <div className="logoText">
+          <div className="logoName"></div>
+          <div className="logoSlogan"></div>
+        </div>
+      </div>
     </header>
   );
 }
